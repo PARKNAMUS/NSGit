2250f4c8aa99a801b77a8f375425642e309b1abd
PARKNAMUS <skatn7979@gmail.com>
2020-07-03 15:07:29 +0900

    upload algorithm124

---
+++ JavaAlgorithm/Algorithm124.java
@@ -0,0 +1,45 @@
+package park.java.ex01;
+
+public class Algorithm124 {
+	/*
+	 * 124 나라가 있습니다. 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다.
+
+124 나라에는 자연수만 존재합니다.
+124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다.
+예를 들어서 124 나라에서 사용하는 숫자는 다음과 같이 변환됩니다.
+
+10진법	124 나라	10진법	124 나라
+1	1	6	14
+2	2	7	21
+3	4	8	22
+4	11	9	24
+5	12	10	41
+자연수 n이 매개변수로 주어질 때, n을 124 나라
+에서 사용하는 숫자로 바꾼 값을 return 하도록 solution 함수를 완성해 주세요.
+	 * */
+    public String solution(int n) {
+        String answer = "";
+        int div = -1;
+        int per = -1;
+        int nums[] = new int[]{1,2,4};
+        StringBuffer buffer = new StringBuffer();
+        while(true) {
+        	
+        	div = (n-1)/3;
+        	per = (n-1)%3;
+        	buffer.append(nums[per]);
+        	n = div;
+        	if(div == 0) {
+        		break;
+        	}
+        	
+        }
+        answer = buffer.reverse().toString();
+        return answer;
+    }
+    public static void main(String[] args) {
+		Algorithm124 algorithm = new Algorithm124();
+		System.out.println(algorithm.solution(10));
+		
+	}
+}
